cb1b940b0ff26cd71918be45dc8b64429783500b
TheFinnishArmy <thefinnisharmy@gmail.com>
2019-11-28 22:05:40 +0200

    URI Creation

--- src/commands/abilityinfo.rs
+++ src/commands/abilityinfo.rs
@@ -1,5 +1,13 @@
 extern crate heck;
+extern crate http;
+
 use heck::TitleCase;
+
+use http::{
+    Request,
+    Response,
+    Uri
+};
 
 use serenity::prelude::*;
 use serenity::model::prelude::*;
@@ -22,6 +30,10 @@ fn abilityinfo(ctx: &mut Context, msg: &Message) -> CommandResult {
 
     println!["{}", message_content];
 
+    let ability_uri = uri_create(message_content);
+
+    println!["{}", ability_uri.uri()];
+
     Ok(())
 }
 
@@ -35,4 +47,24 @@ fn message_process(message: String) -> String {
     message_content = message_content.trim().to_string();
     message_content = message_content.to_title_case();
     message_content.replace(" ", "_")
+}
+
+fn uri_create(name: String) -> Request<()> {
+    let uri_base = "overwatch.wikia.com";
+    let arguments = format!("/api.php?action=query&titles={}&prop=revisions&rvprop=content&format=json", name);
+    let arguments_slice : &str = &arguments[..]; 
+    
+    
+    let custom_uri = Uri::builder()
+        .scheme("http")
+        .authority(uri_base)
+        .path_and_query(arguments_slice)
+        .build()
+        .unwrap();
+
+    Request::builder()
+        .method("GET")
+        .uri(custom_uri)
+        .body(())
+        .unwrap()
 }
